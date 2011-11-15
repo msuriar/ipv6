af4e511589b31c8ada836de43b7cb98e1b646e0b
Murali Suriar <code@suriar.com>
2011-11-15 00:32:49 +0000

    Still prodding...

--- ipv6.py
+++ ipv6.py
@@ -3,9 +3,6 @@
 import doctest
 import re
 
-TOTAL_ADDRESS_CHUNKS = 8
-TOTAL_COLONS = TOTAL_ADDRESS_CHUNKS - 1
-
 class Error(Exception):
   """Base error class."""
 
@@ -21,9 +18,12 @@ class PrefixLengthError(Error):
 class IPv6Prefix(object):
   """IPv6 prefix object. Stores an IPv6 prefix."""
 
-  def __init__(self,prefix):
+  TOTAL_ADDRESS_CHUNKS = 8
+  TOTAL_COLONS = TOTAL_ADDRESS_CHUNKS - 1
+
+  def __init__(self, prefix):
     if '/' in prefix:
-      self.address_str,self.prefix_length_str = prefix.split('/')
+      self.address_str, self.prefix_length_str = prefix.split('/')
     else:
       self.address_str = prefix
       self.prefix_length_str = '128'
@@ -35,7 +35,8 @@ class IPv6Prefix(object):
     self.__validate_prefix_length()
 
   def __validate_address(self):
-    pass
+    if not self.check_characters(address):
+      raise CharacterError('Invalid character found in address.')
 
   def __validate_prefix_length(self):
     if (0 <= int(self.prefix_length_str) <= 128):
@@ -45,20 +46,22 @@ class IPv6Prefix(object):
                               self.prefix_length_str)
 
 
-  def expand_ipv6_address(address):
-    if not check_characters(address):
-      raise CharacterError('Invalid character found in address.')
+  @classmethod
+  def expand_ipv6_address(cls, address):
+    pass
 
-  def calculate_pad_nibbles(address):
-    separated_address = check_valid_abbreviation(address)
+  @classmethod
+  def calculate_pad_nibbles(cls, address):
+    separated_address = cls.check_valid_abbreviation(address)
     if separated_address:
-      current_colons = sum(map(count_colons, separated_address))
-      additional_colons = TOTAL_COLONS - current_colons
+      current_colons = sum(map(cls.count_colons, separated_address))
+      additional_colons = cls.TOTAL_COLONS - current_colons
       padding = r':0' * additional_colons + ':'
       return padding.join(separated_address)
     else: return 0
 
-  def check_valid_abbreviation(address):
+  @classmethod
+  def check_valid_abbreviation(cls, address):
     separated_address = address.split('::')
     if (len(separated_address) == 1) and separated_address[0] == address:
       return False
@@ -67,13 +70,16 @@ class IPv6Prefix(object):
     else:
       raise AbbreviationError('More than one :: in address.')
 
-  def pad_ipv6_address(address):
+  @classmethod
+  def pad_ipv6_address(cls, address):
     pass
 
-  def count_colons(address):
+  @classmethod
+  def count_colons(cls, address):
     return address.count(':')
 
-  def check_characters(address):
-    validRE = re.compile(r'[^A-Fa-f0-9:]')
-    search = validRE.search
+  @classmethod
+  def check_characters(cls, address):
+    RE_VALID_CHARS = re.compile(r'[^A-Fa-f0-9:]')
+    search = RE_VALID_CHARS.search
     return not bool(search(address))
