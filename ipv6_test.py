c5af66350307da74a0345270b081ee65c46b08f0
Murali Suriar <code@suriar.com>
2012-05-19 01:07:11 +0100

    Updated tests to actually call the right methods. Sigh.

--- ipv6_test.py
+++ ipv6_test.py
@@ -7,12 +7,13 @@ class BadAddress(unittest.TestCase):
   def testBadCharacters(self):
     badInputs = ('123.456.789', '2001:/', 'ghi')
     for s in badInputs:
-      self.assertFalse(ipv6.checkCharacters(s))
+      self.assertFalse(ipv6.IPv6Prefix.check_characters(s))
 
   def testInvalidAbbreviation(self):
     badInputs = ('2001::0::0', '::1::1::', '1::::2')
     for s in badInputs:
-      self.assertRaises(ipv6.InvalidAbbreviationError, ipv6.checkValidAbbreviation, s)
+      self.assertRaises(ipv6.AbbreviationError,
+          ipv6.IPv6Prefix.check_valid_abbreviation, s)
 
 
 if __name__ == "__main__":
